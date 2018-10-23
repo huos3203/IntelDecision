3dfb769289deba66b795d32649360e244af1456c
huos3203 <724987481@qq.com>
2018-10-23 18:00:06 +0800

    Initial Commit

---
+++ IntelDecision/IntelDecision.h
@@ -0,0 +1,19 @@
+//
+//  IntelDecision.h
+//  IntelDecision
+//
+//  Created by admin on 2018/10/23.
+//  Copyright © 2018年 clcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntelDecision.
+FOUNDATION_EXPORT double IntelDecisionVersionNumber;
+
+//! Project version string for IntelDecision.
+FOUNDATION_EXPORT const unsigned char IntelDecisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntelDecision/PublicHeader.h>
+
+
